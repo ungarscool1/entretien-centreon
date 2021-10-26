961506c032f4adf78b499c3f75ff391b1565a04b
Léo Godard <leo.godard@epitech.eu>
2021-10-26 18:15:06 +0200

    Add server logs and anti-evade system

---
+++ server/src/interfaces.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+    username: string;
+    ip: string;
+    socketId: string;
+}
+
+export interface Imessage {
+    from: string;
+    message: string;
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,22 @@
 import parser, {arguments} from "./argParser";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { Imessage, IUser } from "./interfaces";
+import ololog from "ololog";
 
 
 class Main {
 
     parsedArgs: arguments;
-    usernames: string[] = [];
+    users: IUser[] = [];
+    log: ololog;
+
+    constructor() {
+        this.log = ololog.configure({
+            time: true,
+            locate: false
+        });
+    }
 
     /**
      * Run the application
@@ -18,21 +28,36 @@ class Main {
         const io = new Server(httpServer, {});
 
         io.on("connection", (socket) => {
-            console.log("New client connected");
-            
-            socket.on("message", (message: string) => {
-                console.log(message);
+            this.log(`New client #${socket.id} (${socket.handshake.address}) connected`);
+
+            socket.on("message", (message: Imessage) => {
+                let user: IUser = this.users.find(u => u.socketId === socket.id);
+                if (user === undefined) {
+                    this.log.red(`User ${message.from} not found`);
+                    return
+                } else if (user.username !== message.from) {
+                    this.log.info(`User ${user.username} tried to publish content with another name (${message.from})`);
+                    message.from = user.username;
+                }
+                this.log(`${message.from} says: ${message.message}`);
+                socket.broadcast.emit("message", message);
             });
             socket.on("register", (message: string) => {
-                if (this.usernames.includes(message)) {
-                    socket.emit("register", "Username already taken");
+                if (this.users.find(user => user.username === message)) {
+                    socket.emit("register", false);
                 } else {
-                    this.usernames.push(message);
-                    socket.emit("register", "Success");
+                    this.users.push({
+                        username: message,
+                        ip: socket.handshake.address,
+                        socketId: socket.id
+                    });
+                    this.log(`User ${message} registered`);
+                    socket.broadcast.emit("message", {from: "Server", message: `${message} has joined the chat`});
+                    socket.emit("register", true);
                 }
             });
         })
-    httpServer.listen(this.parsedArgs.port);
+        httpServer.listen(this.parsedArgs.port);
     }
 }
 
